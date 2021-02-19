d1b2fef8c8d9985e074cd9eafd957ecdc763ca90
vasek <vasia.motnin@gmail.com>
2021-02-19 15:48:25 +0200

    update

---
+++ components/Landing/index.tsx
@@ -0,0 +1,101 @@
+// import { ILandingProps } from ".."
+// import classes from "./style.module.scss"
+// import Video from "./Video"
+// import Description from "./Description"
+// import Comparison from "./Comparison"
+// import Instruction from "./Instruction"
+// import ProductReviews from "./ProductReviews"
+// import Order from "./Order"
+// import CompanyName from "./CompanyName"
+// import ProductImageBlock from "./ProductImageBlock"
+// import BackTimerFirstSection from "./BackTimerFirstSection"
+// import PhoneNumber from "./PhoneNumber"
+// import SectionBuy from "./SectionBuy"
+import classes from "./style.module.scss"
+
+import { IPageData } from "../../interfaces/ISite"
+
+// export interface IColors {
+//   green: string
+//   grey: string
+//   grey1: string
+//   timerGray: string
+//   hr: string
+//   orange: string
+//   orange1: string
+//   textGreenColor: string
+//   blue: string
+//   starsColor: string
+// }
+
+// const colors: IColors = {
+//   green: "#58b958",
+//   grey: "#2f3339",
+//   grey1: "#f7f8f9",
+//   timerGray: "#EFF2F5",
+//   hr: "#DEDFE0",
+//   orange: "#FA9535",
+//   starsColor: "#FA9535",
+//   orange1: "#FF4E00",
+//   textGreenColor: "green",
+//   blue: "#36D0F8",
+// }
+
+export default function Landing(/* props: IPageData */) {
+  // const {
+  //   phoneNumber,
+  //   companyName,
+  //   names,
+  //   mainImages,
+  //   advantages,
+  //   price,
+  //   video,
+  //   descriptionContainer,
+  //   comparison,
+  //   instruction,
+  //   feedbacks,
+  //   // order,
+  // } = props
+
+  // function productImageBlock(nameId?: number, imgId?: number) {
+  //   if (Array.isArray(mainImages) && Array.isArray(names)) {
+  //     return (
+  //       <ProductImageBlock
+  //         name={names[nameId ?? 0]}
+  //         img={mainImages[imgId ?? 0]}
+  //         colors={colors}
+  //       />
+  //     )
+  //   }
+  // }
+
+  return (
+    <div className={classes.main}> data
+      {/* <PhoneNumber>{phoneNumber}</PhoneNumber>
+      <CompanyName name={companyName} backgroundColor={colors.green} />
+      {productImageBlock()}
+      <BackTimerFirstSection colors={colors} price={price} advantages={advantages} />
+      <Video data={video} />
+      <Description data={descriptionContainer} />
+      <Comparison data={comparison} bacColor={colors.grey} buttonColor={colors.orange} />
+      <Instruction data={instruction} />
+      <ProductReviews
+        data={feedbacks}
+        green={colors.green}
+        starsColor={colors.starsColor}
+      />
+      <Order data={order} bacColor={colors.grey} />
+      <CompanyName name={companyName} backgroundColor={colors.green} />
+      {productImageBlock(1)}
+      <SectionBuy colors={colors} price={price} />
+      <PhoneNumber>{phoneNumber}</PhoneNumber> */}
+    </div>
+  )
+}
+
+const last = <T,>(arr: T[]) => {
+  return arr[arr.length - 1]
+}
+
+const l = last(["a", "b", "c"])
+const l2 = last([1, "2", 3])
---
+++ interfaces/ISite.ts
@@ -0,0 +1,91 @@
+export default interface ISite {
+  id: number
+  text: string
+  link: string
+  pageData: IPageData
+}
+
+export interface IPageData {
+  companyName: string // название компании
+  names: string[] // название товара
+  phoneNumber: string // номер телефона
+  advantages: string[] // список приемуществ
+  goodsLeft: string | number // осталось товара
+  slang: string // сленг товара
+  descriptions: string[] // описание товара
+  mainImages: string[] // картинки товара
+  price: IPrice
+  // блок видео
+  video?: {
+    img: string // картинка видео
+    url: string // ссылка на видео
+  }
+  // сравнение
+  comparison?: {
+    title: string
+    comparisons: Icomparison[]
+  }
+  // блок описания
+  descriptionContainer?: {
+    title: string // заголовок
+    img: string // картинка описания
+    text: string // описание
+  }
+  // инструкция использования
+  instruction: {
+    title: string // описание блока инструкции
+    steps: IInstructionStep[]
+  }
+  // Отзывы о товаре
+  feedbacks: IFeedbacks
+  // информация о заказе
+  // order?: IOrder
+  // companyAddress
+}
+
+export interface IPrice {
+  active: number // текущая цена
+  prev?: number // предедущая цена или цена без скидки
+  procent?: number // процент скидки
+  valute: string // грн.
+}
+
+// сравнение
+interface Icomparison {
+  bad: {
+    // плохой вариант
+    img: string
+    description: string
+  }
+  nice: {
+    // хороший вариант
+    img: string
+    description: string
+  }
+}
+
+interface IInstructionStep {
+  img: string
+  description: string
+}
+
+/**
+ * отзыв
+ */
+export interface IFeedback {
+  img: string
+  city: string
+  yearsOld: string
+  name: string
+  feedback: string
+}
+
+/**
+ * параметры секции с отзывами
+ */
+export interface IFeedbacks {
+  // описание блока отзывов
+  // пример "Как им <boild>пользоваться?</boild>" где теги boild обозначают жырный шрифт
+  title: string
+  data: IFeedback[]
+}

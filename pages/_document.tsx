265266ff8e3b9bd5b6e19fb15f604c1c32e814f9
vasek <vasia.motnin@gmail.com>
2021-02-21 17:26:46 +0200

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,7 @@ class MyDocument extends Document {
           />
           <meta
             name='viewport'
-            content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+            content='user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi'
           />
         </Head>
         <body>
